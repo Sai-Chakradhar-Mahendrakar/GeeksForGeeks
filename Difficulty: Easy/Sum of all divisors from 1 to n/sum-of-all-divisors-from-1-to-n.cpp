b7d08c1f33b9dec5fe624ea52931dfddc1dcf641
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-08-18 14:52:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum of all divisors from 1 to n/sum-of-all-divisors-from-1-to-n.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    long long sumOfDivisors(int num)
+    {
+        long long sum = 0;
+        for(long long i=1;i<=num;i++){
+            sum += (num/i)*i;
+        }
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        long long ans  = ob.sumOfDivisors(N);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
