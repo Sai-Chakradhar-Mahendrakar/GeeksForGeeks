a5ddd40b7d79ef14c4f1a5e8c516ce424025ba58
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-08-26 19:45:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Odd or Even/odd-or-even.cpp
@@ -0,0 +1,27 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    string oddEven(int n) {
+        // code here
+        return (n&1)? "odd":"even";
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.oddEven(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
