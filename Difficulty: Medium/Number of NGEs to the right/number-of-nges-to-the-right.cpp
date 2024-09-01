8acd61ddb4f4437607a4508431c542474396dc8a
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-09-01 09:40:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Number of NGEs to the right/number-of-nges-to-the-right.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+
+    vector<int> count_NGE(int n, vector<int> &arr, int queries, vector<int> &indices){
+        //write your code here
+        vector<int> result;
+        for(auto idx: indices){
+            int cnt=0;
+            for(int i=idx+1;i<n;i++){
+                if(arr[idx]<arr[i]){
+                    cnt++;
+                }
+            }
+            result.push_back(cnt);
+        }
+        
+        return result;
+    }
+
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while(t--){
+        //Input
+
+       int n; cin >> n;
+       vector<int> v1(n);
+       for(int i = 0;i<n;i++) cin >> v1[i];
+        int q; cin >> q;
+        vector<int> v2(q);
+        for(int i = 0;i<q;i++) cin >> v2[i];
+
+        Solution obj;
+        vector<int> ans = obj.count_NGE(n,v1,q,v2);
+
+        for(int i = 0;i<ans.size();i++)
+            cout << ans[i] << " ";
+        cout << endl;
+
+        
+        // cout << "~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+// } Driver Code Ends
