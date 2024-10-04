9da28ca512bfd5d697e9ba1e5c2ba558c91c8e27
Sai Chakradhar Rao Mahendrakar <saichakradharrm@gmail.com>
2024-10-04 14:48:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Binary Search Trees/binary-search-trees.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+// {Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    bool isBSTTraversal(vector<int>& arr) {
+        // your code here
+        for(int i=1;i<arr.size();i++){
+            if(arr[i]<=arr[i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> nums(n);
+        for (int i = 0; i < n; i++) {
+            int k;
+            cin >> k;
+            nums[i] = k;
+        }
+
+        Solution ob;
+        bool res = ob.isBSTTraversal(nums);
+        if (res)
+            cout << "True";
+        else
+            cout << "False";
+
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
